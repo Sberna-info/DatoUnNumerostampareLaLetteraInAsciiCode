8500d2823b0461ead9dd97f9bc7cad33d441e7bf
andry <andry@DESKTOP-SENM7HG>
2020-11-17 12:16:04 +0100

    Add project files.

---
+++ DatounNumeroStamparelaLetteraInAsciiCode/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatounNumeroStamparelaLetteraInAsciiCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char carattereascii;
+            int ndecimale;
+            Console.Write("Inserisci un numero dell'ascii code: ");
+            ndecimale = Convert.ToInt32(Console.ReadLine());
+            carattereascii = Convert.ToChar(ndecimale);
+            Console.WriteLine($"Il Numero - {ndecimale}, carattere ASCII - {carattereascii}");
+            Console.ReadLine();
+
+        }
+    }
+}
